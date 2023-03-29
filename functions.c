da8676243f9bf7748aed65d6699ba5b59be2c3a5
NanaKwasi <yeboahkwasi09@gmail.com>
2023-03-29 09:03:23 -0700

    start

--- functions.c
+++ functions.c
@@ -143,7 +143,7 @@ int print_int(va_list types, char buffer[],
 
 	q++;
 
-	return (write_number(is_negative, i, buffer, flags, width, precision, size));
+	return (write_number(is_negative, q, buffer, flags, width, precision, size));
 }
 
 /************************* PRINT BINARY *************************/
